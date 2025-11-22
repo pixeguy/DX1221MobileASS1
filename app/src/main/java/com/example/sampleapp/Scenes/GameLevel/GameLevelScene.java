7f1114608f3dfa5f73b2a1b6fa3dda166a635523
MinhQuan0402 <ngminhquan.2004@gmail.com>
2025-11-22 17:00:56 +0800

    update

---
+++ app/src/main/java/com/example/sampleapp/Scenes/GameLevel/GameLevelScene.java
@@ -0,0 +1,19 @@
+package com.example.sampleapp.Scenes.GameLevel;
+
+import android.graphics.Canvas;
+
+import com.example.sampleapp.mgp2d.core.GameScene;
+
+public class GameLevelScene extends GameScene {
+
+
+    @Override
+    public void onUpdate(float dt) {
+
+    }
+
+    @Override
+    public void onRender(Canvas canvas) {
+
+    }
+}
